0cb485f69c7364b90923a82f7a5f41e8de4d8cdd
Daniel Smau <smaudaniel19@gmail.com>
2021-04-29 19:38:24 +0200

    Creato il progetto e configurato tailwindcss

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/public/styles.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Poetae</title>
+        <meta
+          name="description"
+          content="Un sito web sviluppato per aiutare a conoscere i poeti"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
