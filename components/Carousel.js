7ceef7128de52d2649e4bfbd1f846fe00ce89c63
Daniel Smau <smaudaniel19@gmail.com>
2021-06-11 19:41:44 +0200

    Migliorata la presentazione

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,54 +1,56 @@
 import { useEffect } from "react";
 
 const Carousel = ({ visible = false }) => {
-  let interval = 999999;
+  const interval = 900000;
 
   return (
     <>
       {visible && (
-        <div
-          id="carouselExampleControls"
-          className="carousel slide fullscreen"
-          data-bs-ride="carousel"
-        >
-          <div className="carousel-inner">
-            <div className="carousel-item active" data-bs-interval={interval}>
-              <img src="img/Slide1.jpg" className="d-block w-100" />
+        <div id="carousel" className="pl-11 pr-11">
+          <div
+            id="carouselExampleControls"
+            className="carousel slide fullscreen"
+            data-bs-ride="carousel"
+          >
+            <div className="carousel-inner">
+              <div className="carousel-item active" data-bs-interval={interval}>
+                <img src="img/Slide1.png" className="d-block w-100" />
+              </div>
+              <div className="carousel-item" data-bs-interval={interval}>
+                <img src="img/Slide2.png" className="d-block w-100" />
+              </div>
+              <div className="carousel-item" data-bs-interval={interval}>
+                <img src="img/Slide3.png" className="d-block w-100" />
+              </div>
+              <div className="carousel-item" data-bs-interval={interval}>
+                <img src="img/Slide4.png" className="d-block w-100" />
+              </div>
             </div>
-            <div className="carousel-item" data-bs-interval={interval}>
-              <img src="img/Slide2.jpg" className="d-block w-100" />
-            </div>
-            <div className="carousel-item" data-bs-interval={interval}>
-              <img src="img/Slide3.jpg" className="d-block w-100" />
-            </div>
-            <div className="carousel-item" data-bs-interval={interval}>
-              <img src="img/Slide4.jpg" className="d-block w-100" />
-            </div>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleControls"
+              data-bs-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleControls"
+              data-bs-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Next</span>
+            </button>
           </div>
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleControls"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleControls"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
         </div>
       )}
     </>
