a8e768256aa76b3cb68799f2c56d061189096377
Smau <smaudaniel19@gmail.com>
2023-09-18 12:17:06 +0200

    .

--- pages/error.js
+++ pages/error.js
@@ -2,7 +2,7 @@ const Error = () => {
   return (
     <div className="flex flex-col justify-center items-center w-full h-screen font-serif">
       <h1 className="text-4xl mb-4">Il server non è al momento disponibile.</h1>
-      <small className="text-2xl text-gray-300">Torneremo presto!</small>
+      <small className="text-2xl text-gray-300">The project is no longer maintained</small>
     </div>
   );
 };
